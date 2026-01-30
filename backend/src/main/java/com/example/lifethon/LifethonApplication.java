a54bd82d8de992e49c4c15a170c74175bb0432a3
YiJun Zou <zapboltisepic@gmail.com>
2026-01-30 17:30:32 +1100

    added initial spring boot initialisation

---
+++ backend/src/main/java/com/example/lifethon/LifethonApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifethon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifethonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifethonApplication.class, args);
+	}
+
+}
