d365bdb345dd65a9c717a4a35b9e2869a1665b49
YiJun Zou <zapboltisepic@gmail.com>
2026-02-04 05:10:03 +1100

    added User repository

---
+++ backend/src/main/java/com/example/lifethon/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.lifethon.repository;
+
+import com.example.lifethon.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
